c6f8a7a43eb44de25c4183e92c6aa11388dddd40
Chinthaka Dilshan <dilshanperera200325712934@gmail.com>
2024-04-14 14:37:34 +0530

    Implement Item service package

---
+++ src/main/java/org/eCommerce/service/ItemService.java
@@ -0,0 +1,13 @@
+package org.eCommerce.service;
+
+import org.eCommerce.dto.Item;
+import org.eCommerce.entity.ItemEntity;
+
+import java.util.List;
+
+public interface ItemService {
+    void addItem(Item item);
+    List<ItemEntity> getAllItems();
+    boolean deleteItem(Long id);
+    void updateItem(Item item);
+}
---
+++ src/main/java/org/eCommerce/service/impl/ItemServiceImpl.java
@@ -0,0 +1,47 @@
+package org.eCommerce.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.eCommerce.dto.Item;
+import org.eCommerce.entity.ItemEntity;
+import org.eCommerce.repository.ItemRepository;
+import org.eCommerce.service.ItemService;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class ItemServiceImpl implements ItemService {
+
+    final ItemRepository repository;
+
+    ModelMapper mapper;
+
+    @Bean
+    public void setup(){this.mapper = new ModelMapper();}
+    @Override
+    public void addItem(Item item) {
+        ItemEntity entity = mapper.map(item, ItemEntity.class);
+        repository.save(entity);
+    }
+
+    @Override
+    public List<ItemEntity> getAllItems() {
+        return  (List<ItemEntity>) repository.findAll();
+    }
+
+    @Override
+    public boolean deleteItem(Long id) {
+        if(repository.existsById(id)){
+            repository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void updateItem(Item item) {
+
+    }
+}
