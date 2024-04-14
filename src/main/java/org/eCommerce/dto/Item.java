d52331624748cf77ba6bdcd155922e6d0ae716fe
Chinthaka Dilshan <dilshanperera200325712934@gmail.com>
2024-04-14 14:12:51 +0530

    Implement Item dto class

---
+++ src/main/java/org/eCommerce/dto/Item.java
@@ -0,0 +1,17 @@
+package org.eCommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Item {
+    private Long id;
+    private String name;
+    private String description;
+    private String category;
+    private int qty;
+    private String imageUrl;
+}
